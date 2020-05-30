8c4f3086fd364a02516f7183357afed20ecb59ad
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-05-30 14:19:41 +0200

    Create subarrayPowers.cpp

---
+++ algorithmic problems/subarrayPowers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+// This program checks the all the powers of K in the array
+
+void solve(){
+  int K; 
+  std::vector<int> nums = {2, 4, 8, 81, 64, 1024, 9};
+  std::vector<int> kPows;
+  int max = nums[0];
+  for (int i = 1; i < nums.size(); i++){
+    if(nums[i] > max){
+      max = nums[i];
+    }
+  }
+  std::cout << "K value : ";
+  std::cin >> K;
+  for (int i = 1; i <= max; i++){
+    kPows.push_back(pow(K, i));
+  }
+  for (int i = 0; i < kPows.size(); i++){
+     for (int j = 1; j < nums.size(); j++){
+       if(kPows[i] == nums[j]){
+       std::cout << "{"<< nums[j] << "}" << std::endl;
+        break;
+       }
+     }
+  }
+}
+
+int main(){
+  solve();
+  return 0;
+}
