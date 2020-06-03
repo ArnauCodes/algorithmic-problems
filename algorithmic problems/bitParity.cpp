8fbce26b3f6d7434b0c81068ccd4dbcf9c2ca72e
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-06-03 17:34:00 +0200

    Update bitParity.cpp

--- algorithmic problems/bitParity.cpp
+++ algorithmic problems/bitParity.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
+/* Given an unsigned integer, find it's parity. 
+Parity of a number refers to whether it contains an odd or even number of 1-bits. The number has “odd parity”, 
+if it contains odd number of 1-bits and is “even parity” if it contains even number of 1-bits */
+
 void solve(unsigned int n){
   unsigned int count = 0;
-  while(n){
-    count += n & 1;
-    n >>=1;
-  }
+  while(n != 0){
+    if (n % 2 == 1){
+      count++;
+    }
+    n = n / 2;
+  } 
   if (count % 2 != 0){
-    std::cout << "Odd" << "\n";
+    std::cout << "Odd" << std::endl;
   } else {
-    std::cout << "Even" << "\n";
+    std::cout << "Even" << std::endl;
   }
 }
 
