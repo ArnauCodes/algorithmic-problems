9e6da22dabcbfd8e131b1c7cc6c243877b91a252
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-06-07 20:08:39 +0200

    Create patternJumping.cpp

---
+++ algorithmic problems/patternJumping.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+void solve(int nJumps){
+  double l = log2(nJumps);
+  floor(l) == ceil(l) ? std::cout << "True" << "\n" : std::cout << "False" << "\n";
+}
+
+int main() {
+  int nJumps = 89;
+  solve(nJumps);
+  return 0;
+}
