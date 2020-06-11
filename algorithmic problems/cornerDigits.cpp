4ff579933908516f726c5478e13bea08e8a3f178
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-06-11 21:08:43 +0200

    Update cornerDigits.cpp

--- algorithmic problems/cornerDigits.cpp
+++ algorithmic problems/cornerDigits.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+/* Given a number N and the task is to print the first and last digit of N. */
+
 void solve(int n){
   std::string sN = std::to_string(n);
   int l = sN.length();
