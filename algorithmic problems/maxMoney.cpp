e95134637efcf226e18a35143862e75bdf6a43bf
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-06-19 19:33:33 +0200

    Create maxMoney.cpp

---
+++ algorithmic problems/maxMoney.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*Given street of houses (a row of houses), each house having some amount of money kept inside; 
+now there is a thief who is going to steal this money but he has a constraint/rule that he cannot 
+steal/rob two adjacent houses. Find the maximum money he can rob. */
+
+
+void solve(int n, int money){
+  n % 2 == 0 ? std::cout << (n / 2) * money : std::cout <<(n / 2 + 1) * money;
+}
+
+int main() {
+  int n;
+  int money;
+  std::cin >> n >> money;
+  solve(n, money);
+  return 0;
+}
