8834a37004f000bf7741d92312c28eb18ae423cc
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-06-19 17:39:33 +0200

    Create kClosestPointsTotheOrigin.cpp

---
+++ algorithmic problems/kClosestPointsTotheOrigin.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+/* We have a list of points on the plane.  Find the K closest points to the origin (0, 0). */ 
+
+// Just when I uploaded this I came up with a O(nlogn) answer I might upload in the future 
+
+
+
+void kClosest(std::vector<std::vector<int>> points, int K){
+  std::vector<double> toEvaluate;
+  int n = points.size(), ts;
+  if(K > n || K < 1) throw;
+  for (int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if (points[i][j + 1] != 0){
+       toEvaluate.push_back(sqrt(pow(points[i][j], 2) + pow(points[i][j + 1], 2)));
+      }
+    }
+  }
+  ts = toEvaluate.size();
+  std::sort(toEvaluate.begin(), toEvaluate.end());
+  for (int p = 0; p < K; p++){
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < n; j++){
+        if (points[i][j + 1] != 0 && sqrt(pow(points[i][j], 2) + pow(points[i][j + 1], 2)) == toEvaluate[p]){
+          std::cout << "{" << points[i][j] << ", " << points[i][j + 1] << "}"<< std::endl;
+        }
+      }
+    }
+  }
+}
+
+// X, Y cords
+int main() {
+  std::vector<std::vector<int>> pointsToChoose = {{3, 3}, {5, -1}, {-2, 4}};
+  int K = 2; 
+  kClosest(pointsToChoose, K);
+  return 0;
+}
